73468bc67dceadf9e5008870e39273ada788f7d3
Jonatan Pietroski <jonatan.santos@dextra-sw.com>
2020-08-10 15:34:45 -0300

    feature/321 - Get C Nome do meu commit

--- web/services/MainService.js
+++ web/services/MainService.js
@@ -1,19 +1,21 @@
-(function(){
+(function () {
   'use strict';
 
+  app.factory('MainService', [
+    '$http',
+    function ($http) {
+      var response = {};
 
-  app.factory('MainService', ['$http', function($http){
+      //route that returns a list of random people.
+      response.getPeople = function () {
+        return $http.get('http://localhost:8090/rest/people');
+      };
+      //route that returns a list of random people.
+      response.getC = function () {
+        return $http.get('http://localhost:8090/rest/people');
+      };
 
-    var response = {};
-
-    //route that returns a list of random people.
-    response.getPeople = function(){
-      return $http.get('http://localhost:8090/rest/people');
-    };
-
-    return response;
-
-
-  }]);
-
+      return response;
+    },
+  ]);
 })();
